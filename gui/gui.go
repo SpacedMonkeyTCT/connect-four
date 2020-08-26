bed63cbc273deffc90b018bb28b000cf157e9d18
Ben Ramsay <spacedmonkeytct@gmail.com>
2020-08-26 12:21:29 +0100

    Refactor how new chip is created in GUI

--- gui/gui.go
+++ gui/gui.go
@@ -21,71 +21,65 @@ type connectFour interface {
 
 type GUI struct {
 	game            connectFour
-	win             *pixelgl.Window
+	window          *pixelgl.Window
 	board           *board
-	player          int
-	currentChip     *chip
 	redChipFactory  chipFactory
 	blueChipFactory chipFactory
 	holdY           float64
+	currentChip     *chip
 }
 
 func New(game connectFour) *GUI {
 	columns := game.Columns()
 	rows := game.Rows()
-	win := newWindow(columns, rows, tileSize)
-	winHeight := win.Bounds().H()
-	holdY := winHeight - float64(3*tileSize/4)
-
+	window := newWindow(columns, rows, tileSize)
 	t := NewTileSet("tiles.png", tileSize)
-	b := NewBoard(win, columns, rows, t.GetBoardTile())
-	rcf := NewChipFactory(win, t.GetRedChip())
-	bcf := NewChipFactory(win, t.GetBlueChip())
 
-	g := GUI{
+	g := &GUI{
 		game:            game,
-		win:             win,
-		board:           b,
-		redChipFactory:  rcf,
-		blueChipFactory: bcf,
-		currentChip:     rcf.New(),
-		holdY:           holdY,
+		window:          window,
+		board:           NewBoard(window, columns, rows, t.GetBoardTile()),
+		redChipFactory:  NewChipFactory(window, t.GetRedChip()),
+		blueChipFactory: NewChipFactory(window, t.GetBlueChip()),
+		holdY:           window.Bounds().H() - float64(3*tileSize/4),
 	}
+	g.currentChip = g.newChip(game.CurrentPlayer())
 
-	return &g
+	return g
 }
 
 func (g *GUI) ProcessInput() {
 	if column := g.board.CheckForMove(); column > 0 {
 
 		if row := g.game.MakeMove(column); row > 0 {
-			g.addCurrentChipToBoard(column, row)
-			g.player = g.game.CurrentPlayer()
-
-			if g.player == redPlayer {
-				g.newRedChip()
-			} else {
-				g.newBlueChip()
-			}
+			g.board.AddChip(g.currentChip, column, row)
+			g.currentChip = g.newChip(g.game.CurrentPlayer())
 		}
 	}
 }
 
 func (g *GUI) Update() {
-	x := g.win.MousePosition().X
+	x := g.window.MousePosition().X
 	pos := pixel.V(x, g.holdY)
 	g.currentChip.SetPos(pos)
 }
 
 func (g GUI) Draw() {
-	g.win.Clear(colornames.Skyblue)
+	g.window.Clear(colornames.Skyblue)
 	g.board.Draw()
 	g.currentChip.Draw()
-	g.win.Update()
+	g.window.Update()
 }
 
 func (g GUI) Closed() bool {
-	return g.win.Closed()
+	return g.window.Closed()
+}
+
+func (g GUI) newChip(player int) *chip {
+	if player == redPlayer {
+		return g.redChipFactory.New()
+	}
+	return g.blueChipFactory.New()
 }
 
 func newWindow(columns, rows, tileSize int) *pixelgl.Window {
@@ -94,21 +88,9 @@ func newWindow(columns, rows, tileSize int) *pixelgl.Window {
 		Bounds: pixel.R(0, 0, float64(tileSize*(columns+1)), float64(tileSize*(rows+2))),
 		VSync:  true,
 	}
-	win, err := pixelgl.NewWindow(cfg)
+	window, err := pixelgl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
-	return win
-}
-
-func (g *GUI) newRedChip() {
-	g.currentChip = g.redChipFactory.New()
-}
-
-func (g *GUI) newBlueChip() {
-	g.currentChip = g.blueChipFactory.New()
-}
-
-func (g *GUI) addCurrentChipToBoard(column, row int) {
-	g.board.AddChip(g.currentChip, column, row)
+	return window
 }
